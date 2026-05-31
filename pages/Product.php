276392fc1695bfbc765830b013900dba9214947d
= <=>
2026-06-01 00:34:50 +0100

    Porduct 70% finie

---
+++ pages/Product.php
@@ -0,0 +1,450 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+    />
+    <link rel="stylesheet" href="../assets/css/output.css">
+    <link rel="stylesheet" href="../assets/css/product.css">
+</head>
+<body>
+    <?php include "../includes/header.php" ?>
+    <section>
+        <aside>
+            <form action="">
+                <div class="filter">
+                    <div class="heading">
+                        <div>
+                            <i class="fa-solid fa-sliders"></i>
+                            <h3>Filtres</h3>
+                        </div>
+                        <div>
+                            <input type="reset" name="" id="" value="Reinistialiser">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="category">
+                    <div class="heading">
+                        <div>
+                            <i class="fa-solid fa-layer-group"></i>
+                            <h3>Catégories</h3>
+                        </div>
+                        <p>^</p>
+                    </div>
+                    <div class="list-categorie list">
+                        <ul>
+                            <li>
+                                <div>
+                                    <input type="checkbox" name="" id="">
+                                    Cahiers
+                                </div>
+                                <span class="number-product">35</span>
+                            </li>
+                            <li>
+                                <div>
+                                    <input type="checkbox" name="" id="">
+                                    Cahiers
+                                </div>
+                                <span class="number-product">35</span>
+                            </li>
+                            <li>
+                                <div>
+                                    <input type="checkbox" name="" id="">
+                                    Cahiers
+                                </div>
+                                <span class="number-product">35</span>
+                            </li>
+                            <li>
+                                <div>
+                                    <input type="checkbox" name="" id="">
+                                    Cahiers
+                                </div>
+                                <span class="number-product">35</span>
+                            </li>
+                            <li>
+                                <div>
+                                    <input type="checkbox" name="" id="">
+                                    Cahiers
+                                </div>
+                                <span class="number-product">35</span>
+                            </li>
+                            <li>
+                                <div>
+                                    <input type="checkbox" name="" id="">
+                                    Cahiers
+                                </div>
+                                <span class="number-product">35</span>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+
+                <div class="prix">
+                    <div class="heading">
+                        <div>
+                            <i class="fa-solid fa-dollar-sign"></i>
+                            <h3>Prix</h3>
+                        </div>
+                        <p>^</p>
+                    </div>
+                    <div>
+                        <div>
+                            <input type="range" name="" id="range1">
+
+                        </div>
+                        <div>
+                            <div><span>0</span> <span>dt</span></div>
+                            -
+                            <div><span>50</span> <span>dt</span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="marque">
+                    <div class="heading">
+                        <div>
+                            <i class="fa-solid fa-tag"></i>
+                            <h3>Marque</h3>
+                        </div>
+                        <p>^</p>
+                    </div>
+                    <div class="list-marque list">
+                        <ul>
+                            <li>
+                                <div>
+                                    <input type="checkbox" name="" id="">
+                                    Cahiers
+                                </div>
+                                <span class="number-product">35</span>
+                            </li>
+                            <li>
+                                <div>
+                                    <input type="checkbox" name="" id="">
+                                    Cahiers
+                                </div>
+                                <span class="number-product">35</span>
+                            </li>
+                            <li>
+                                <div>
+                                    <input type="checkbox" name="" id="">
+                                    Cahiers
+                                </div>
+                                <span class="number-product">35</span>
+                            </li>
+                            <li>
+                                <div>
+                                    <input type="checkbox" name="" id="">
+                                    Cahiers
+                                </div>
+                                <span class="number-product">35</span>
+                            </li>
+                            <li>
+                                <div>
+                                    <input type="checkbox" name="" id="">
+                                    Cahiers
+                                </div>
+                                <span class="number-product">35</span>
+                            </li>
+                            <li>
+                                <div>
+                                    <input type="checkbox" name="" id="">
+                                    Cahiers
+                                </div>
+                                <span class="number-product">35</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="disponibility">
+                    <div class="heading">
+                        <div>
+                            <i class="fa-brands fa-font-awesome"></i>
+                            <h3>Disponibilité</h3>
+                        </div>
+                        <p>^</p>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="" id=""> 
+                        <label for=""> En stock uniquement</label>
+                    </div>
+                </div>
+            </form>
+        </aside>
+
+
+
+        <main>
+            <div class="information">
+                <p>Affichage de 1 a 12 sur 120 produits</p>
+                <div>
+                    Trier par 
+                    <select name="" id="">
+                        <option value="" selected>none</option>
+                        <option value="">Popularity</option>
+                        <option value="">prix</option>
+                        <option value="">nom</option>
+                        <option value="">type</option>
+                    </select>
+                </div>
+            </div>
+
+            <!--phase des articles-->
+            <div class="articles">
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="https://www.alkirtas.com/65769-large_default/stylo-bille-bic-cristal-soft-pochette-de-10.jpg" alt="">
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <p class="title">
+                        Cahier oxford 192 pages grand format
+                    </p>
+                    <p class="price">
+                        3,250 Dt
+                    </p>
+                    <button><span>🛒</span> Ajouter au panier</button>
+                </article>
+            </div>
+        </main>
+    </section>
+</body>
+</html>
