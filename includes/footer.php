94f4b75d087be1d1ff02c9d0fec9effaff52e41f
= <=>
2026-06-03 00:23:11 +0100

    Completed footer

---
+++ includes/footer.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="../assets/css/footer.css">
+</head>
+<body>
+    <footer>
+        <div class="container-footer">
+            <div class="part-one">
+                <div class="colonne">
+                    <img src="../assets/images/footer/img1.png" alt="">
+                    <p>Notre librairie en ligne spécialisée dans la fourniture scolaire et de bureau .
+                        Qualité, choix et prix au rendez-vous!
+                    </p>
+                    <div class="info">
+                        <i class="fa-regular fa-envelope"></i>
+                        <div class="textContainer">
+                            <h3>Email</h3>
+                            <p>librairiechebbi4@gmail.com</p>
+                        </div>
+                    </div>
+                    <div class="info">
+                        <i class="fa-solid fa-phone"></i>
+                        <div class="textContainer">
+                            <h3>Telephone</h3>
+                            <p>+216 21 123 456</p>
+                        </div>
+                    </div>
+                    <div class="info">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <div class="textContainer">
+                            <h3>Adresse</h3>
+                            <p>Cite Rommena , nouvelle medina Ben arous tunis</p>
+                        </div>
+                    </div>
+                    <div class="info">
+                        <i class="fa-regular fa-clock"></i>
+                        <div class="textContainer">
+                            <h3>Horaires d'ouverture</h3>
+                            <p>Lun - Sam : 8h30 - 19h00</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="colonne">
+                    <h2>Lien Rapides</h2>
+                    <ul>
+                        <li><i class="fa-solid fa-angle-right"></i><a href="#">Acueil</a></li>
+                        <li><i class="fa-solid fa-angle-right"></i><a href="#">A propos de nous</a></li>
+                        <li><i class="fa-solid fa-angle-right"></i><a href="#">Nos produits</a></li>
+                        <li><i class="fa-solid fa-angle-right"></i><a href="#">Contactez-nous</a></li>
+                    </ul>
+                </div>
+
+                <div class="colonne">
+                    <h2>Categories</h2>
+                    <ul>
+                        <li><a href="#"><span>🎒</span> Cartables & Sacs</a></li>
+                        <li><a href="#"><span>✏️</span> Stylo & Ecriture</a></li>
+                        <li><a href="#"><span>📒</span> Cahier & Copies</a></li>
+                        <li><a href="#"><span>🖌️</span> Beaux-Arts</a></li>
+                        <li><a href="#"><span>🗂️</span> Fournitures de Bureau</a></li>
+                        <li><a href="#"><span>🧮</span> Calculatrices</a></li>
+                        <li><a href="#"><span>📏</span> Geometrie & Maths</a></li>
+                        <li><a href="#"><span>📖</span> Lecture & Dictionnaires</a></li>
+                    </ul>
+                </div>
+
+                <div class="colonne">
+                    <h2>Lien Utiles</h2>
+                    <ul>
+                        <li><i class="fa-solid fa-angle-right"></i><a href="#"><span>Mon compte</span></a></li>
+                        <li><i class="fa-solid fa-angle-right"></i><a href="#"><span>Suivi de commande</span></a></li>
+                    </ul>
+                </div>
+
+                <div class="colonne">
+                    <h2>Suivez-Nous</h2>
+                    <ul class="social-media">
+                        <li><a href="#">insta</a></li>
+                        <li><a href="#">facebook</a></li>
+                    </ul>
+                    <hr>
+                    <h2>Restez informée</h2>
+                    <p>Inscrivez vous sur notre site web pour recevoir nos offres et nouveautés</p>
+                    <div class="info">
+                        <i class="fa-solid fa-truck-fast"></i>
+                        <div class="textContainer">
+                            <h3>Livraison Rapide</h3>
+                            <p>Partout en tunisie</p>
+                        </div>
+                    </div>
+                    <div class="info">
+                        <i class="fa-solid fa-circle-check"></i>
+                        <div class="textContainer">
+                            <h3>Produits de qualités</h3>
+                            <p>Selectionnées pour vous</p>
+                        </div>
+                    </div>
+                    <div class="info">
+                        <i class="fa-solid fa-sack-dollar"></i>
+                        <div class="textContainer">
+                            <h3>Paiement sur Livraison</h3>
+                            <p>Payée votre colis au livraison</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+            <hr>
+            <div class="part-two">
+                <div class="image">
+                    <img src="../assets/images/footer/img2.png" alt="">
+                </div>
+                <div class="textContainer">
+                    <h3>&copy; Librairie Chebbi . Tous droits réservés.  </h3>
+                    <p>Concu avec ❤️ en Tunisie</p>
+                </div>
+                <div class="image">
+                    <img src="../assets/images/footer/img2.png" alt="">
+                </div>
+            </div>
+
+
+
+        </div>
+
+
+    </footer>
+</body>
+</html>
