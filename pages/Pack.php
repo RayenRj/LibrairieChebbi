88e34e3411f35f767b2cbfb5bca80313f6e23594
= <=>
2026-06-02 18:31:07 +0100

    page pack 90% kemlet ne9sha el responsive

---
+++ pages/Pack.php
@@ -0,0 +1,462 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Packs</title>
+    <link rel="stylesheet" href="../assets/css/output.css">
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+    />
+    <link rel="stylesheet" href="../assets/css/pack.css">
+</head>
+<body>
+    
+    <!-- include ll header -->
+    <?php include("../includes/header.php");?>
+
+
+
+
+    <div class="regulateur-header-imageContainer-espace">
+        <div class="image-container boxwidth">
+            <img src="../assets/images/pack/pack.png" alt="packs image">
+            <div class="text">
+                <h1>🎒Packs Scolaires Complets 2026</h1>
+                <p>Trouvez le pack adapté a chaque niveau scolaire et economisez <span>jusqu'à 20%</span></p>
+                <div class="button">
+                    <a href="">Voir les Packs</a>
+                    <a href="">Decouvrir Promotions</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="card-container boxwidth" >
+        <div class="card">
+            <img src="../assets/images/pack/calc.png" alt="">
+            <h2>Primaire</h2>
+            <a href="#pack-primaire"><span>Voir les packs</span> ⭢</a>
+        </div>
+        <div class="card">
+            <img src="../assets/images/pack/palette.png" alt="">
+            <h2>Collège</h2>
+            <a href="#pack-college"><span>Voir les packs</span> ⭢</a>
+        </div>
+        <div class="card">
+            <img src="../assets/images/pack/book.png" alt="">
+            <h2>Secondaire</h2>
+            <a href="#pack-secondaire"><span>Voir les packs</span> ⭢</a>
+        </div>
+        <div class="card">
+            <img src="../assets/images/pack/hat.webp" alt="">
+            <h2>Bac</h2>
+            <a href="#pack-bac"><span>Voir les packs</span> ⭢</a>
+        </div>
+    </div>
+    <!-- endof card part -->
+
+
+
+    <!-- start of pack part -->
+     <section class="packs-part boxwidth">
+
+        <div class="pack-primaire packs-container" id="pack-primaire">
+            <div class="text">
+                <img src="../assets/images/pack/palette.png" alt="">
+                <h2>Packs Primaire</h2>
+            </div>
+            <main>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack1.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack1.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack1.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack1.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack1.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack1.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+            </main>
+        </div>
+        <!-- end of primary part -->
+
+
+
+
+        <!-- colluege part  -->
+
+        <div class="pack-college packs-container" id="pack-college">
+            <div class="text">
+                <img src="../assets/images/pack/palette.png" alt="">
+                <h2>Packs Primaire</h2>
+            </div>
+            <main>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack2.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack2.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack2.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack2.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack2.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack2.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+            </main>
+        </div>
+
+
+
+
+
+
+
+
+
+        <div class="pack-secondaire packs-container" id="pack-secondaire">
+            <div class="text">
+                <img src="../assets/images/pack/palette.png" alt="">
+                <h2>Packs Primaire</h2>
+            </div>
+            <main>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack3.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack3.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack3.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack3.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack3.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack3.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+            </main>
+        </div>
+
+
+
+
+
+
+
+    <div class="pack-bac packs-container" id="pack-bac">
+            <div class="text">
+                <img src="../assets/images/pack/palette.png" alt="">
+                <h2>Packs Primaire</h2>
+            </div>
+            <main>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack1.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack1.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack1.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack1.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack1.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="image">
+                        <img src="../assets/images/pack/pack_images/pack1.png" alt="">
+                    </div>
+                    <div class="text-pack">
+                        <h2>Pack primaire</h2>
+                        <p>25 articles</p>
+                        <p class="price">59 Dt</p>
+                    </div>
+                    <div class="button-pack">
+                        <a href="">Voir Details</a>
+                        <a href=""><i class="fa-solid fa-cart-arrow-down"></i> Ajouter au panier</a>
+                    </div>
+                </article>
+            </main>
+        </div>
+
+
+     </section>
+     end of all packs
+</body>
+</html>
