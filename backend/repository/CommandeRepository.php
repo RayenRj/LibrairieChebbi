3acd485f4faec17f81ceecd19c01a4ef3d49f02b
RayenRj <rjibi.rayen01@gmail.com>
2026-06-20 02:52:56 +0100

    kamelna commandeRepository w bdina fl productRepository

--- backend/repository/CommandeRepository.php
+++ backend/repository/CommandeRepository.php
@@ -1,4 +1,6 @@
 <?php 
+    // Commande Repository : tous les methodes relative ll commande bch nest7a9ouhom fl khedma
+
     require_once(__DIR__ . "/IRepository.php");
     require_once(__DIR__ . "/Repository.php");
     require_once(__DIR__ . "../models/Commande.php");
@@ -16,6 +18,8 @@
             $stmt->execute();
             return $stmt->fetch(PDO::FETCH_NUM)[0];
         }
+
+        // statistique relative au mois courant
         public function nombreTotaleCommandesCeMois(){
             $query = "select count(*) from commande where month(date_commande) = month(current_date()) and year(date_commande) = year(curdate());";
             $stmt = $this->db->prepare($query);
@@ -23,19 +27,70 @@
             return $stmt->fetch(PDO::FETCH_NUM)[0];
         }
 
-        public function nombreTotaleCommandesConfirméeCeMois(){
-            $query = "select count(*) from commande where statut = 'confirmée' and month(date_commande) = month(current_date()) and year(date_commande) = year(curdate());";
+
+        /**
+         * *
+         * @param string $statut => statut ei bch ta3ml 3leha el filtrage
+         * @return array liste feha les commandes filtrer selon la  critére
+         */
+        public function nombreTotaleCommandeCeMois(string $statut){
+            $query = "select count(*) from commande where statut = '?' and month(date_commande) = month(current_date()) and year(date_commande) = year(curdate());";
             $stmt = $this->db->prepare($query);
-            $stmt->execute();
+            $stmt->execute([$statut]);
             return $stmt->fetch(PDO::FETCH_NUM)[0];
         }
-        public function nombreTotaleCommandesAnnuléeCeMois(){
-            $query = "select count(*) from commande where statut = 'confirmée' and month(date_commande) = month(current_date()) and year(date_commande) = year(curdate());";
+
+        // statistique en generales
+        public function FiltreCommandesParStatut(string $statut){
+            $query = "select * from {$this->tName} where statut = '?' ;";
             $stmt = $this->db->prepare($query);
-            $stmt->execute();
-            return $stmt->fetch(PDO::FETCH_NUM)[0];
+            $stmt->execute([$statut]);
+            $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $res;
         }
-        // kamel el commande repository
+
+        // recherche de commande :
+        // recherche par une seule critere 
+        /**
+         * Summary of filtreCommandeParDate
+         * @param string $dateDebut si la valeur est null donc bch tetna7a ml filtrage
+         * @param string $dateFin si la valeur est null donc bch tetna7a ml filtrage
+         * @return array lista feha 
+         */
+        public function filtreCommandeParDate(string $dateDebut , string $dateFin){
+            if ($dateDebut == null && $dateFin==null){return null;}
+            else if($dateDebut == null){
+                $query = "select * from {$this->tName} where date_commande < ? ;";
+                $stmt = $this->db->prepare($query);
+                $stmt->execute([$dateFin]);
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+            else if($dateFin== null){
+                $query = "select * from {$this->tName} where date_commande > ? ;";
+                $stmt= $this->db->prepare($query);
+                $stmt->execute([$dateDebut]);
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+            else{
+                $query = "select * from {$this->tName} where date_commande > ? and date_commande < ?";
+                $stmt = $this->db->prepare($query);
+                $stmt->execute([$dateDebut , $dateFin]);
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+        }
+        /**
+         * Summary of filtreCommandePartielle
+         * @param string $critere => el critere eli bch ysir 3leha el filtrage
+         * @param string $data => el data eli bch nesta3melha reellement fl filtrage
+         * @return array liste of filtreed data
+         */
+        public function filtreCommandePartielle(string $critere , string $data){
+            $query = "select * from {$this->tName} where ? like '%?%'";
+            $stmt = $this->db->prepare($query);
+            $stmt->execute([$critere, $data]);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
 
 
     }
