2a19a076d89319ba9518cfb26a93ba81901a826f
RayenRj <rjibi.rayen01@gmail.com>
2026-06-22 16:57:16 +0100

    correction du fishier commandeRepository

--- backend/repository/CommandeRepository.php
+++ backend/repository/CommandeRepository.php
@@ -41,7 +41,7 @@
             $stmt = $this->db->prepare($query);
             return $stmt->execute([$id_commande]);
         }
-        public function getCommandeById($id_commande): bool{
+        public function getCommandeById($id_commande){
             $query = "select * from commande where id_commande = ? ;";
             $stmt = $this->db->prepare($query);
             $stmt->execute([$id_commande]);
@@ -50,7 +50,74 @@
 
         // partie search 
 
+    /**
+         * *
+         * @param string $statut => statut ei bch ta3ml 3leha el filtrage
+         * @return array liste feha les commandes filtrer selon la  critére
+         */
+        public function nombreTotaleCommandeCeMois(string $statut){
+            $query = "select count(*) from commande where statut = '?' and month(date_commande) = month(current_date()) and year(date_commande) = year(curdate());";
+            $stmt = $this->db->prepare($query);
+            $stmt->execute([$statut]);
+            return $stmt->fetch(PDO::FETCH_NUM)[0];
+        }
+
+        // statistique en generales
+        public function FiltreCommandesParStatut(string $statut){
+            $query = "select * from {$this->tName} where statut = '?' ;";
+            $stmt = $this->db->prepare($query);
+            $stmt->execute([$statut]);
+            $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $res;
+        }
+
+        // recherche de commande :
+        // recherche par une seule critere 
+        /**
+         * Summary of filtreCommandeParDate
+         * @param string $dateDebut si la valeur est null donc bch tetna7a ml filtrage
+         * @param string $dateFin si la valeur est null donc bch tetna7a ml filtrage
+         * @return array lista feha 
+         */
+        public function filtreCommandeParDate(string $dateDebut , string $dateFin){
+            if ($dateDebut == null && $dateFin==null){return null;}
+            else if($dateDebut == null){
+                $query = "select * from {$this->tName} where date_commande < ? ;";
+                $stmt = $this->db->prepare($query);
+                $stmt->execute([$dateFin]);
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+            else if($dateFin== null){
+                $query = "select * from {$this->tName} where date_commande > ? ;";
+                $stmt= $this->db->prepare($query);
+                $stmt->execute([$dateDebut]);
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+            else{
+                $query = "select * from {$this->tName} where date_commande > ? and date_commande < ?";
+                $stmt = $this->db->prepare($query);
+                $stmt->execute([$dateDebut , $dateFin]);
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+        }
+        /**
+         * Summary of filtreCommandePartielle
+         * @param string $critere => el critere eli bch ysir 3leha el filtrage
+         * @param string $data => el data eli bch nesta3melha reellement fl filtrage
+         * @return array liste of filtreed data
+         */
+        public function filtreCommandePartielle(string $critere , string $data){
+            $query = "select * from {$this->tName} where ? like '%?%'";
+            $stmt = $this->db->prepare($query);
+            $stmt->execute([$critere, $data]);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+
+
 
     }
 
 ?>
+
+
