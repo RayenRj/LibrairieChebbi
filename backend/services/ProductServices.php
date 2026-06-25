f2a90012e1afae25535c31e16e34294e5d7a531c
= <=>
2026-06-25 19:54:06 +0100

    El services 9rib tekmel : just ne9sa el method rechercher Article fl ProductService w ba3d nabdew fl controllers + routing

--- backend/services/ProductServices.php
+++ backend/services/ProductServices.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once(__DIR__ . "/../repository/ProductRepository.php");
+include_once(__DIR__ . "/../exception/*.php");
 include_once(__DIR__ . "/../models/Product.php");
 include_once(__DIR__ . "/../exception/IdentifiantInvalideException.php");
 
@@ -10,16 +11,75 @@ class ProductServices{
         $this->productRepo = new ProductRepository();
     }
     
-    public function createProduct($data){}
-    public function getAllProduct() : array {}
-    public function deleteProduct(int $idProduit){}
-    public function checkProductExist(int $idProduit) : bool {}
-    public function faireRemise(int $idProduit , float $remiseAmount){}
-    public function getProductById(int $id){}
-    public function updateProduct(int $idProduit , array $data) : bool {}
-    public function increaseStock(int $idProduit , int $quantite) : bool {}
-    public function getFinalPrice(int $idProduit){}
+    // la seule restante
+    public function modifierArticle(){
+
+    }
+    //done
+    public function rechercherArticle(string $categorie , string $libelle , float $prixMax , float $prixMin=0 , string $stock , string $trie , int $limit = 10 , int $page = 0){
+        return $this->productRepo->rechercherArticle($categorie , $libelle, $prixMax,$prixMin , $stock , $trie , $limit,$page);
+    }
+    //done
+    public function createProduct(string $libelle,float $prixUnitaire,int $quantite ,string $categorie,string $marque,float $remise ,string $description,string $image_url ,?string $codeBarre =""): bool{
+        if(empty($libelle)){throw new Exception("Libelle de produit ne doit pas etre vide!");}
+        if($prixUnitaire<0){throw new Exception("prixUnitaire de produit ne doit pas etre negatif!");}
+        if($quantite<0){throw new Exception("quantite de produit ne doit pas etre negatif!");}
+        if(empty($categorie)){throw new Exception("categorie de produit ne doit pas etre vide!");}
+        if(empty($marque)){throw new Exception("marque de produit ne doit pas etre vide!");}
+        if(empty($image_url)){throw new Exception("image_url de produit ne doit pas etre vide!");}
+        if($remise < 0 || $remise > $prixUnitaire){throw new Exception("remise de produit invalide!");}
+        return $this->productRepo->createNewProduct($libelle, $prixUnitaire, $quantite , $categorie, $marque, $remise , $description, $image_url , $codeBarre);
+    }
+    //done
+    public function getAllProduct() : ?array {
+        return $this->productRepo->findAllProducts();
+    }
+    //done
+    public function deleteProduct(int $idProduit):bool{
+        if($idProduit<1){throw new IdentifiantInvalideException("l'id doit etre positif");}
+        return $this->productRepo->deleteProduct($idProduit);
+    }
+    //done
+    public function checkProductExist(int $idProduit) : bool {
+        if($idProduit<1){throw new IdentifiantInvalideException("l'id doit etre positif");}
+        return $this->productRepo->findProduitById($idProduit) !== null;
+    }
+    //done
+    public function faireRemise(int $idProduit , float $remiseAmount) : bool{
+        if($idProduit<1){throw new IdentifiantInvalideException("l'id doit etre positif");}
+        if($remiseAmount<0){throw new Exception("La remise doit etre positif!");}
+        $product = $this->productRepo->findProduitById($idProduit);
+        if($product["prix"] < $remiseAmount){return false;}
+        return $this->productRepo->ajouterUnRemiseSurProduit($idProduit, $remiseAmount);
+    }
+    //done
+    public function getProductById(int $id) : array{
+        if($id <1){throw new IdentifiantInvalideException("l'id doit etre positif");}
+        return $this->productRepo->findProduitById($id);
+    }
+    //done
+    public function increaseStock(int $idProduit , int $quantite) : bool {
+        if($idProduit<1){throw new IdentifiantInvalideException("l'id doit etre positif");}
+        if($quantite<0){throw new QuantityException("Quantity doit etre > 0");}
+        return $this->productRepo->increaseQuantity($idProduit,$quantite);
+    }
+    //done
+    public function decreaseStock(int $idProduit , int $quantite) : bool {
+        if($idProduit<1){throw new IdentifiantInvalideException("l'id doit etre positif");}
+        if($quantite<0){throw new QuantityException("Quantity doit etre > 0");}
+        $product = $this->productRepo->findProduitById($idProduit);
+        if($product["quantite_stock"] < $quantite){throw new Exception("La quantite est superieur a la quantite en stock");}
+        return $this->productRepo->decreaseQuantity($idProduit,$quantite);
+    }
+    //done
+    public function getFinalPrice(int $idProduit) : float {
+        if($idProduit<1){throw new IdentifiantInvalideException("l'id doit etre positif");}
+        $product = $this->productRepo->findProduitById($idProduit);
+        if($product == null){throw new Exception("Article not found!");}
+        return ($product["prix"] - $product["remise"]) ;
+    }
 }
+
 
 
 
